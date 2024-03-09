06658074093c0ba967ecb2e0dba4083ff09a2477
Huy Hoang <boydaihungst@gmail.com>
2024-03-09 15:37:57 +0000

    auto push sub 2024-03-09 15:37:38.393083883 +0000

--- extract_sub.sh
+++ extract_sub.sh
@@ -12,7 +12,7 @@ TIME_STAMP="$(date +"%Y-%m-%d %H:%M:%S.%N +0000")"
 touch --date "$(cat $BASE_FOLDER/backup/AnimeSubtitles/last_extract_sub)" -m "$BASE_FOLDER/backup/AnimeSubtitles/last_extract_sub"
 
 SEARCH_DIRS=("$BASE_FOLDER/Anime" "$BASE_FOLDER/Movie")
-for sub_folder in ${SEARCH_DIRS[@]}; do
+for sub_folder in "${SEARCH_DIRS[@]}"; do
 	if [[ ! -d "$sub_folder" ]]; then
 		continue
 	fi
@@ -23,17 +23,17 @@ for sub_folder in ${SEARCH_DIRS[@]}; do
 			continue
 		fi
 		# Find out which tracks contain the subtitles
-		mkvmerge -J "$filename" | jq -c '.tracks[] | select(.type=="subtitles" and (.properties.language=="vie" or .properties.language=="und"))' | while read -r subline; do
+		# mkvmerge -J "$filename" | jq -c '.tracks[] | select(.type=="subtitles" and (.properties.language=="vie" or .properties.language=="und"))' | while read -r subline; do
+		ffprobe -show_streams -print_format json -loglevel quiet "$filename" | jq -c '.streams[] | select (.codec_type=="subtitle" and (.tags.language=="vie" or .tags.language=="und" or .tags.language==null))' | while read -r subline; do
 			# Grep the number of the subtitle track
-			tracknumber=$(echo "$subline" | jq -c ".id")
-			language=$(echo "$subline" | jq -c ".properties.language")
-			codec=$(echo "$subline" | jq -c ".codec")
+			tracknumber=$(echo "$subline" | jq -c ".index")
+			codec=$(echo "$subline" | jq -c ".codec_name")
 			file_extension=""
-			if [[ $codec == *"SubRip/SRT"* ]]; then
+			if [[ $codec == "\"subrip\"" ]]; then
 				file_extension="srt"
-			elif [[ $codec == *"SubStationAlpha"* ]] || [[ $codec == *"AdvancedSub"* ]]; then
+			elif [[ $codec == "\"ass\"" ]] || [[ $codec == "\"ssa\"" ]]; then
 				file_extension="ass"
-			elif [[ $codec == *"HDMV PGS"* ]]; then
+			elif [[ $codec == "\"hdmv_pgs_subtitle\"" ]]; then
 				file_extension="sup"
 			fi
 			# Get base name for subtitle
@@ -44,11 +44,12 @@ for sub_folder in ${SEARCH_DIRS[@]}; do
 				echo "$codec" >>"$BASE_FOLDER/backup/AnimeSubtitles/unknown_sub_type.log"
 				continue
 			fi
+      TEMP_SUB_FILE="$BASE_FOLDER/backup/AnimeSubtitles/.temp_sub"
 			# Extract the track to a .tmp file
 			{
-				mkvextract tracks "$filename" --redirect-output "$BASE_FOLDER/backup/AnimeSubtitles/extract_error.log" -q "$tracknumber":"$subtitlename.vie.$file_extension.bak" && mv "$subtitlename.vie.$file_extension.bak" "$subtitlename.vie.$file_extension"
+				(mkvextract tracks "$filename" --redirect-output "$BASE_FOLDER/backup/AnimeSubtitles/extract_error.log" -q "$tracknumber":"$TEMP_SUB_FILE.vie.$file_extension" || ffmpeg -y -nostdin -loglevel panic -i "$filename" -map 0:"$tracknumber" "${TEMP_SUB_FILE}.vie.${file_extension}") && mv "$TEMP_SUB_FILE.vie.$file_extension" "$subtitlename.vie.$file_extension"
 			} || {
-				rm "$subtitlename.vie.$file_extension.bak"
+				rm "$TEMP_SUB_FILE.vie.$file_extension"
 			}
 		done
 	done
